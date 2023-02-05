16cc1fbb9bbb15f245b4d849b51260aa91a58b72
Kardian <kuszawet@gmail.com>
2023-02-05 13:50:45 +0100

    basic layout

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="container">
+      <h1 className="title text-center">Inventory List</h1>
+      <form className="d-flex" style={{justyContent:"center"}}>
+        <div className="form-group m-1">
+          <input type="text" placeholder="Item name" style={{border:"1px solid #7633f9"}}/>
+        </div>
+        <div className="form-group m-1">
+          <input type="text" placeholder="Item quantity" style={{border:"1px solid #7633f9"}}/>
+        </div>
+        <button className="btn btn-primary">Add</button>
+      </form>
+      <ul className="container text-left" style={{width:420}}>
+        <li>list name quantity</li>
+      </ul>
     </div>
   );
 }
